4644cb2132ce2da0468425f763bb07d0abd20258
Lukas Hahn <lukas1h07@gmail.com>
2024-02-04 15:47:29 -0800

    Fixed Text

--- frontend/app/ui/compoents/Footer/index.tsx
+++ frontend/app/ui/compoents/Footer/index.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="shadow-large flex items-center justify-center p-4">
             <div className=" w-full flex items-center justify-between max-w-[1000px]">
                 <Link href="/contact-us">Contact Us</Link>
-                <Link href="/order">Contact Us</Link>
+                <Link href="/order">Order</Link>
             </div>
         </div>
     )
