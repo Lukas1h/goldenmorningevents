3b26156e7414fb498da918ed00b025561b1ebbec
Lukas Hahn <lukas1h07@gmail.com>
2024-02-04 16:17:02 -0800

    All sorts of stuff

--- frontend/app/ui/compoents/Footer/index.tsx
+++ frontend/app/ui/compoents/Footer/index.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <div className="shadow-large flex items-center justify-center p-4">
-            <div className=" w-full flex items-center justify-between max-w-[1000px]">
-                <Link href="/contact-us">Contact Us</Link>
-                <Link href="/order">Order</Link>
+        <div className="shadow-large flex items-center justify-center pt-2 pb-2">
+            <div className=" w-full grid grid-cols-3 max-w-[1000px] place-items-center text-xs md:text-medium">
+                <Link href="/contact-us" className="text-gray-500 text-xs md:text-medium">Contact Us</Link>
+                <p>Golden Morning</p>
+                <Link href="/order" className="text-gray-500 text-xs md:text-medium">Order</Link>
             </div>
         </div>
     )
