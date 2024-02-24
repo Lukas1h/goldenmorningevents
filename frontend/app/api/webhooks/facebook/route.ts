9976e6e6ab8c7b4ab14b361d84c5abc4649dfced
Lukas Hahn <lukas1h07@gmail.com>
2024-02-24 11:11:16 -0800

    Add logging

--- frontend/app/api/webhooks/facebook/route.ts
+++ frontend/app/api/webhooks/facebook/route.ts
@@ -1,18 +1,23 @@
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const mode = searchParams.get('hub.mode')
+    console.log(`got url ${request.url}`)
 
     if(mode == "subscribe"){
         //Subscribe handler
         const id = searchParams.get('hub.challenge')
         const verify_token = searchParams.get('hub.verify_token')
+        
 
-        if (verify_token != "Swn7448810"){
-            return Response.json({error:`Invalid hub.verify_token: ${verify_token}`},{status:401})
-        }
+        // if (verify_token != "Swn7448810"){
+        //     console.log(`Invalid hub.verify_token: ${verify_token}`)
+        //     console.log(`Needs to be ${"Swn7448810"}`)
 
+        //     return Response.json({error:`Invalid hub.verify_token: ${verify_token}`},{status:401})
+        // }
+        console.log(`All good returning id ${id}`)
         return new Response(id)
     }
-    
+    console.log(`Unknown mode: ${mode}`)
     return Response.json({error:`Unknown mode: ${mode}`},{status:500})
   }
